8c5e8ddac01c5ddbfe77e18c962eed0914c95f33
Documenter.jl <documenter@juliadocs.github.io>
2021-09-15 22:57:52 +0000

    build based on 5d73082

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = cowsay","category":"page"},{"location":"#cowsay","page":"Home","title":"cowsay","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for cowsay.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [cowsay]","category":"page"}]
+}
