dbab38833359a6311bb819ef6a8f614ac5ec8db4
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-11-23 11:26:40 -0600

    Add vader-koala cowfile
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

---
+++ src/cows/vader-koala.cow.jl
@@ -0,0 +1,20 @@
+##
+## Another canonical koala?
+##
+function vader_koala(;eyes="oo", tongue="  ", thoughts="\\")
+the_cow =
+"""
+   $thoughts
+    $thoughts        .
+     .---.  //
+    Y|o o|Y//
+   /_(i=i)K/
+   ~()~*~()~
+    (_)-(_)
+
+     Darth
+     Vader
+     koala
+"""
+return the_cow
+end
