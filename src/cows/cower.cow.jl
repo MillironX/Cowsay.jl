e3bb2f322f903951208f7146c3a813f457e43e3b
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-11-23 11:40:16 -0600

    Add cower cowfile
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

---
+++ src/cows/cower.cow.jl
@@ -0,0 +1,18 @@
+##
+## A cowering cow
+##
+function cower(;eyes="oo", tongue="  ", thoughts="\\")
+the_cow =
+"""
+     $thoughts
+      $thoughts
+        ,__, |    |
+        (oo)\\|    |___
+        (__)\\|    |   )\\_
+             |    |_w |  \\
+             |    |  ||   *
+
+             Cower....
+"""
+return the_cow
+end
