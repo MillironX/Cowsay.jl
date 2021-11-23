281e75511f9368b9bfeea132542c0ec9b853d799
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-11-23 11:27:55 -0600

    Add Vader cowfile
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

---
+++ src/cows/vader.cow.jl
@@ -0,0 +1,17 @@
+##
+## Cowth Vader, from geordan@csua.berkeley.edu
+##
+function vader(;eyes="oo", tongue="  ", thoughts="\\")
+the_cow =
+"""
+        $thoughts    ,-^-.
+         $thoughts   !oYo!
+          $thoughts /./=\\.\\______
+               ##        )\\/\\
+                ||-----w||
+                ||      ||
+
+               Cowth Vader
+"""
+return the_cow
+end
