2355d02dab5b4427f423bb44a7a97b3058abc98e
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-11-23 11:44:25 -0600

    Add elephant cowfile
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

---
+++ src/cows/elephant.cow.jl
@@ -0,0 +1,19 @@
+##
+## An elephant out and about
+##
+function elephant(;eyes="oo", tongue="  ", thoughts="\\")
+the_cow =
+"""
+ $thoughts     /\\  ___  /\\
+  $thoughts   // \\/   \\/ \\\\
+     ((    O O    ))
+      \\\\ /     \\ //
+       \\/  | |  \\/
+        |  | |  |
+        |  | |  |
+        |   o   |
+        | |   | |
+        |m|   |m|
+"""
+return the_cow
+end
