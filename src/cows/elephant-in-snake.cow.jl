880618346b884dac843b9cbe284ba5673a221538
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-11-23 11:46:59 -0600

    Add elephant-in-snake cowfile
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

---
+++ src/cows/elephant-in-snake.cow.jl
@@ -0,0 +1,17 @@
+##
+## Do we need to explain this?
+##
+function elephant_in_snake(;eyes="oo", tongue="  ", thoughts="\\")
+the_cow = """
+   $thoughts
+    $thoughts              ....
+           ........    .
+          .            .
+         .             .
+.........              .......
+..............................
+
+Elephant inside ASCII snake
+"""
+return the_cow
+end
