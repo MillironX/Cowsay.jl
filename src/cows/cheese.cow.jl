0127742ed46352bc1ac5a32937fc75a7e8fe3c83
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-11-23 11:39:29 -0600

    Add cheese cowfile
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

---
+++ src/cows/cheese.cow.jl
@@ -0,0 +1,27 @@
+##
+## The cheese from milk & cheese
+##
+function cheese(;eyes="oo", tongue="  ", thoughts="\\")
+the_cow =
+"""
+   $thoughts
+    $thoughts
+      _____   _________
+     /     \\_/         |
+    |                 ||
+    |                 ||
+   |    ###\\  /###   | |
+   |     0  \\/  0    | |
+  /|                 | |
+ / |        <        |\\ \\
+| /|                 | | |
+| |     \\_______/   |  | |
+| |                 | / /
+/||                 /|||
+   ----------------|
+        | |    | |
+        ***    ***
+       /___\\  /___\\
+"""
+return the_cow
+end
