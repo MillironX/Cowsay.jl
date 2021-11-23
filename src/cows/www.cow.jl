67f66876c15c9eb20a3ca73fe2b045026a36a57c
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-11-23 11:24:56 -0600

    Add www cowfile
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

---
+++ src/cows/www.cow.jl
@@ -0,0 +1,14 @@
+##
+## A cow wadvertising the World Wide Web, from lim@csua.berkeley.edu
+##
+function www(;eyes="oo", tongue="  ", thoughts="\\")
+$the_cow =
+"""
+        $thoughts   ^__^
+         $thoughts  ($eyes)\\_______
+            (__)\\       )\\/\\
+             $tongue ||--WWW |
+                ||     ||
+"""
+return the_cow
+end
