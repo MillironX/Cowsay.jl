26fba4a469d2f600f5d456d7abba2ab419bf0f9c
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-11-23 11:38:23 -0600

    Add bunny cowfile
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

---
+++ src/cows/bunny.cow.jl
@@ -0,0 +1,14 @@
+##
+## A cute little wabbit
+##
+function bunny(;eyes="oo", tongue="  ", thoughts="\\")
+the_cow =
+"""
+  $thoughts
+   $thoughts   \\
+        \\ /\\
+        ( )
+      .( o ).
+"""
+return the_cow
+end
