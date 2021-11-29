58d672f066d9d1eb7605c5b356a27823fca0f7bb
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-11-29 08:02:01 -0600

    Remove doc script
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- make-docs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-julia --project=docs -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.instantiate()'
-julia --project=docs -e 'using Documenter: DocMeta, doctest; using Cowsay; DocMeta.setdocmeta!(Cowsay, :DocTestSetup, :(using Cowsay); recursive=true); doctest(Cowsay)'
-julia --project=docs docs/make.jl
