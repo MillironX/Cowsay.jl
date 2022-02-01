aeaba5e27725c840d34ac9458091392bb4831f76
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-02-01 16:11:03 -0600

    Remove verbose testing for 1.0 compat

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Documenter
 
 DocMeta.setdocmeta!(Cowsay, :DocTestSetup, :(using Cowsay); recursive=true)
 
-@testset verbose = true "cowsay" begin
+@testset "cowsay" begin
     @testset "Doctests" begin
         doctest(Cowsay)
     end
