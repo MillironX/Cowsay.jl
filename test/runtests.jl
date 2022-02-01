b068326b2e8d4c8ccaa1149d8867c6d88a4768db
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-02-01 15:42:15 -0600

    Reorganize tests into testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,9 @@ using Test
 using Documenter
 
 DocMeta.setdocmeta!(Cowsay, :DocTestSetup, :(using Cowsay); recursive=true)
-doctest(Cowsay)
+
+@testset verbose = true "cowsay" begin
+    @testset "Doctests" begin
+        doctest(Cowsay)
+    end
+end
